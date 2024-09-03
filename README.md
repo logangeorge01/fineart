6802b00e277d27868da02733b159b1c9842b57bd
Logan George <logangeorge@microsoft.com>
2024-09-03 15:47:09 -0700

    way to validate images source correctly and make imageset configurable

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ A Chrome extension that shows a random famous painting in your new tab
 https://chrome.google.com/webstore/detail/fineart/hphaegpjmiodcafgoadjfgfpjjpklnfb
 
 9 current users
+
+Fineart can be sideloaded by cloning the repo, going to chrome://extensions, loading unpacked and selecting the fineart repo. By doing this you can use any imageset by changing the link in js/newtab.js.
+
+Validate your images are loading properly by opening validate.html
---
+++ js/validate.js
@@ -0,0 +1,41 @@
+window.onload = () => {
+	fetch('https://raw.githubusercontent.com/logangeorge01/fineart/logangeorge/dynamic-links/js/artwork.json')
+		.then(response => response.json())
+		.then(artworks => {
+			const gallery = document.getElementById('gallery');
+
+			// Set up the grid properties
+			const columns = Math.ceil(Math.sqrt(artworks.length)); // Square root to create a nearly square grid
+			gallery.style.gridTemplateColumns = `repeat(${columns}, 1fr)`; // Create a grid with dynamic columns
+
+			artworks.forEach(artwork => {
+				const [title, artist, year, url] = artwork;
+
+				const container = document.createElement('div');
+				container.style.display = 'flex';
+				container.style.flexDirection = 'column';
+				container.style.alignItems = 'center';
+				container.style.justifyContent = 'center';
+
+				const img = document.createElement('img');
+				img.src = url;
+				img.alt = title;
+				img.style.maxWidth = '100%';
+				img.style.maxHeight = '100px'; // Set the max height to keep images small
+				img.style.objectFit = 'contain';
+
+				const caption = document.createElement('div');
+				caption.textContent = `${title} (${artist}, ${year})`;
+				caption.style.color = 'white';
+				caption.style.fontSize = '12px';
+				caption.style.textAlign = 'center';
+				caption.style.marginTop = '5px';
+
+				container.appendChild(img);
+				container.appendChild(caption);
+				gallery.appendChild(container);
+			});
+
+		})
+		.catch(error => console.error('Error fetching the JSON file:', error));
+}
